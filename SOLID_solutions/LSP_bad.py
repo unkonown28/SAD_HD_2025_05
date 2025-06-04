7d42dbca7e2ec6b391fe8fc70c90819372968db5
prashanth sersani <sersaniprashanth@gmail.com>
2025-06-05 01:14:01 +0200

    Delete SOLID_solutions directory

--- SOLID_solutions/LSP_bad.py
+++
@@ -1,7 +0,0 @@
-class Bird:
-    def fly(self):
-        print("Flying")
-
-class Ostrich(Bird):
-    def fly(self):
-        raise Exception("Ostriches can't fly!")
--- SOLID_solutions/OCP_bad.py
+++
@@ -1,8 +0,0 @@
-class Discount:
-    def get_discount(self, customer_type):
-        if customer_type == "regular":
-            return 10
-        elif customer_type == "vip":
-            return 20
-        else:
-            return 0
