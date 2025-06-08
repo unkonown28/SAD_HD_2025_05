f8a7b1dbfd1eac59eb86abf5e7ad39636df5d5c5
prashanth sersani <sersaniprashanth@gmail.com>
2025-06-08 23:23:16 +0200

    Update ISP_01.java

--- SOLID/ISP_01.java
+++ SOLID/ISP_01.java
@@ -1,39 +1,27 @@
 package SOLID;
 
-public class ISP_01 {
+/**
+ * 👇 Originally, one big interface included scan and fax methods.
+ * We now separate functionality for simpler printers.
+ */
+interface PrintService {
+    void print();
+}
 
-    /*
-     * TASK:
-     * This SimplePrinter is not a multi functional printer but a very
-     * simple one.
-     * Please fix this to obey the ISP (Interface Segregation Principle)!
-     */
-    
-    public static interface Printer {
-        void printDocument();
-        void scanDocument();
-        void faxDocument();    
-    }
-
-    public static class SimplePrinter implements Printer {
-        @Override
-        public void printDocument() {
-            System.out.println("Sending document to the printer ...");
-        }
-
-        @Override
-        public void scanDocument() {
-            throw new UnsupportedOperationException("Unimplemented method 'scanDocument'");
-        }
-
-        @Override
-        public void faxDocument() {
-            throw new UnsupportedOperationException("Unimplemented method 'faxDocument'");
-        }        
-    }
-
-    public static void main(String[] args) {
-        SimplePrinter printer = new SimplePrinter();
-        printer.printDocument();
+/**
+ * 👇 This printer only implements what it actually supports.
+ * No need to throw UnsupportedOperationException anymore.
+ */
+class InkjetPrinter implements PrintService {
+    public void print() {
+        System.out.println("Print job running.");
     }
 }
+
+public class ISP_01 {
+    public static void main(String[] args) {
+        InkjetPrinter printer = new InkjetPrinter();
+        printer.print();
+    }
+}
+
