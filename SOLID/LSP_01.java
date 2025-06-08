b489df0ebea0130d7bc5d800c8cfbc32082bb1c3
prashanth sersani <sersaniprashanth@gmail.com>
2025-06-08 23:21:30 +0200

    Update LSP_01.java

--- SOLID/LSP_01.java
+++ SOLID/LSP_01.java
@@ -1,62 +1,44 @@
 package SOLID;
 
+/**
+ * 👇 Rectangle class that works with width and height.
+ * Originally, Square extended Rectangle and broke behavior when overriding methods.
+ */
+class Shape2D {
+    protected int w, h;
+
+    Shape2D(int w, int h) {
+        this.w = w;
+        this.h = h;
+    }
+
+    int area() {
+        return w * h;
+    }
+}
+
+/**
+ * 👇 Instead of subclassing, Square now has its own structure.
+ * This prevents accidental misuse of setters.
+ */
+class Box {
+    private int side;
+
+    Box(int s) {
+        side = s;
+    }
+
+    int area() {
+        return side * side;
+    }
+}
+
 public class LSP_01 {
-
-    /*
-     * TASK:
-     * (classical example)
-     *
-     * The Square class does not behave like a Rectangle when
-     * setting the width and/or height. Please fix this to obey
-     * the LSP (Liskov Substitution Principle).
-     */
-    
-    public static class Rectangle {
-        protected int width;
-        protected int height;
-
-        public Rectangle(int width, int height) {
-            setWidth(width);
-            setHeight(height);
-        }
-
-        public void setWidth(int width) {
-            this.width = width;
-        }
-
-        public void setHeight(int height) {
-            this.height = height;
-        }
-
-        public int getArea() {
-            return width * height;
-        }
-    }
-
-    public static class Square extends Rectangle {
-        public Square(int width) {
-            super(width, width);
-        }
-
-        @Override
-        public void setWidth(int width) {
-            this.width = width;
-            this.height = width;
-        }
-
-        @Override
-        public void setHeight(int height) {
-            this.width = height;
-            this.height = height;
-        }
-    }
-
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(5, 10);
-        System.out.println("Rectangle Area: " + rectangle.getArea());
+        Shape2D r = new Shape2D(4, 5);
+        Box sq = new Box(4);
 
-        Square square = new Square(0);
-        square.setWidth(5);
-        System.out.println("Square Area: " + square.getArea());
+        System.out.println("Rectangle: " + r.area());
+        System.out.println("Square: " + sq.area());
     }
 }
