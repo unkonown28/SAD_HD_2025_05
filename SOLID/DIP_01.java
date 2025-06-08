92af49fdb62a26e296692fc5686a99bf63650ed9
prashanth sersani <sersaniprashanth@gmail.com>
2025-06-08 23:25:00 +0200

    Update DIP_01.java

--- SOLID/DIP_01.java
+++ SOLID/DIP_01.java
@@ -1,38 +1,46 @@
 package SOLID;
 
-public class DIP_01 {
+/**
+ * 👇 We introduce an interface so that Switch works with any Device, not just LightBulb.
+ */
+interface Device {
+    void on();
+    void off();
+}
 
-    /*
-     * TASK:
-     * The Switch is violating the DIP (Dependency Inversion Principle).
-     * Please fix this!
-     */
-    
-    public static class LightBulb {
-        public void turnOn() {
-            System.out.println("Light is ON!");
-        }
-        public void turnOff() {
-            System.out.println("Light is OFF!");
-        }
+/**
+ * 👇 Lamp is a concrete implementation of the Device interface.
+ */
+class Lamp implements Device {
+    public void on() {
+        System.out.println("Lamp lit.");
     }
 
-    public static class Switch {
-        private LightBulb lightBulb;
-
-        // this is "Dependency Injection" (composition style)
-        public Switch(LightBulb lightBulb) {
-            this.lightBulb = lightBulb;
-        }
-
-        public void operate() {
-            lightBulb.turnOn();
-        }
-    }
-
-    public static void main(String[] args) {
-        LightBulb lightBulb = new LightBulb();
-        Switch lightSwitch = new Switch(lightBulb);
-        lightSwitch.operate();
+    public void off() {
+        System.out.println("Lamp dark.");
     }
 }
+
+/**
+ * 👇 Switch now depends on abstraction (Device), not a specific Lamp class.
+ */
+class Toggle {
+    private final Device dev;
+
+    public Toggle(Device dev) {
+        this.dev = dev;
+    }
+
+    public void press() {
+        dev.on(); // Abstraction call
+    }
+}
+
+public class DIP_01 {
+    public static void main(String[] args) {
+        Device l = new Lamp();
+        Toggle t = new Toggle(l);
+        t.press();
+    }
+}
+
